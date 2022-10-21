50005fe76c106b0fee142a3776f0df5e8cc44b32
volhaTaler <olyatarelka@gmail.com>
2022-10-21 08:42:43 +0200

    Working version of project too.

---
+++ main/src/main/java/com/tutego/date4u/controller/ProfileController.java
@@ -0,0 +1,100 @@
+package com.tutego.date4u.controller;
+
+
+import com.tutego.date4u.core.config.CurrentUser;
+import com.tutego.date4u.core.dto.ProfileFormData;
+import com.tutego.date4u.core.photo.Photo;
+import com.tutego.date4u.core.profile.Profile;
+import com.tutego.date4u.core.profile.ProfileRepository;
+import com.tutego.date4u.core.profile.UnicornRepository;
+import com.tutego.date4u.service.ProfileService;
+import com.tutego.date4u.service.UnicornService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class ProfileController {
+    
+    @Autowired
+    private final ProfileRepository profiles;
+    
+    @Autowired
+    private ProfileService profileService;
+    
+    @Autowired
+    private UnicornService unicornService;
+    
+    
+    private final Logger log = LoggerFactory.getLogger( getClass() );
+    
+    public ProfileController(final ProfileRepository profiles, UnicornRepository unicorns) {
+        this.profiles = profiles;
+    }
+    
+    @RequestMapping( "/profile" )
+    public String profilePage(Authentication auth, Model model) {
+        CurrentUser unicorn = null;
+        if(auth != null) {
+            unicorn = (CurrentUser) auth.getPrincipal();
+            log.info(unicorn.getUsername() + ":  " + unicorn.getPassword());
+            Optional<Profile> currentProfile= unicornService.getNicknameByEmail(unicorn.getUsername());
+            Profile temp = currentProfile.get();
+            //List<Photo> photos = temp.getPhotos();
+            ProfileFormData pfd = ProfileFormData.createPFD(temp);
+            List<String> photos = pfd.getPhotos();
+            if(photos == null || photos.isEmpty()){
+                model.addAttribute("profilePhoto", "default");
+            }else{
+                model.addAttribute("profilePhoto", photos.get(0));
+            }
+            
+            model.addAttribute("profile", pfd);
+            model.addAttribute("photosList", photos);
+            return "profile";
+        }
+        
+        return "/login?logout";
+    }
+    @RequestMapping( "/profile/{id}" )
+    public String profilePage(@PathVariable("id") long id, Model model) {
+        Optional<Profile> profile = profiles.findById(id);
+        if(profile.isEmpty()){
+            return "redirect:/search?";
+        }
+        Profile temp = profile.get();
+        model.addAttribute("profile", ProfileFormData.createPFD(temp));
+        
+        return "redirect:/profile/" + id;
+    }
+    
+    @PostMapping( value = "/save" , params = "action=save")
+    public String saveProfile( @ModelAttribute("profile") ProfileFormData profile ) {
+        log.info( profile.toString() );
+        Optional<Profile> toUpdateProfile = profiles.findById(profile.getId());
+        if(toUpdateProfile.isPresent()){
+            Profile tmp = toUpdateProfile.get();
+            profile.updateProfile(tmp);
+            profiles.save(tmp);
+            
+        }else{
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "This profile does not exist.");
+            
+        }
+        
+        return "redirect:/profile?success";
+    }
+    
+}
---
+++ main/src/main/java/com/tutego/date4u/core/dto/PhotoFormData.java
@@ -0,0 +1,101 @@
+package com.tutego.date4u.core.dto;
+
+import com.tutego.date4u.core.photo.Photo;
+import com.tutego.date4u.core.profile.Profile;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.NotEmpty;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhotoFormData {
+    
+    private Long id;
+    
+    private Profile profile;
+    
+    @NotEmpty
+    private String name;
+    private boolean isProfilePhoto;
+    
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime created;
+    
+    public PhotoFormData(){}
+    
+    public PhotoFormData(long id, Profile profile, String name, boolean isProfilePhoto, LocalDateTime created) {
+        this.id = id;
+        this.profile = profile;
+        this.name = name;
+        this.isProfilePhoto = isProfilePhoto;
+        this.created = created;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public Profile getProfile() {
+        return profile;
+    }
+    
+    public void setProfile(Profile profile) {
+        this.profile = profile;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public boolean isProfilePhoto() {
+        return isProfilePhoto;
+    }
+    
+    public void setProfilePhoto(boolean profilePhoto) {
+        isProfilePhoto = profilePhoto;
+    }
+    
+    public LocalDateTime getCreated() {
+        return created;
+    }
+    
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+    
+    
+    public Photo generateNewPhoto(){
+        return new Photo(this.id, this.profile, this.name, this.isProfilePhoto, this.created);
+    }
+    
+    public void setAsProfilePhoto(Photo photo){
+        
+        if(this.isProfilePhoto()) {
+            photo.setProfilePhoto(true);
+        }else{
+            photo.setProfilePhoto(false);
+        }
+    }
+    
+    public static List<PhotoFormData> createListOfPhotosPFD(List<Photo> photos){
+        List<PhotoFormData> resultList =  new ArrayList<>();
+        for (Photo p: photos) {
+            resultList.add(new PhotoFormData(p.getId(), p.getProfile(), p.getName(), p.isProfilePhoto(), p.getCreated()));
+        }
+        return resultList;
+    }
+   
+}
