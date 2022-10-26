81fa7a6cdd063391b506e4860cd58a0ef6a81939
volhaTaler <olyatarelka@gmail.com>
2022-10-26 20:36:46 +0200

    Working version

--- main/src/main/java/com/tutego/date4u/controller/Date4uWebController.java
+++ main/src/main/java/com/tutego/date4u/controller/Date4uWebController.java
@@ -1,5 +1,6 @@
 package com.tutego.date4u.controller;
 
+import com.tutego.date4u.core.config.CurrentUser;
 import com.tutego.date4u.core.dto.FilterFormData;
 import com.tutego.date4u.core.dto.ProfileFormData;
 import com.tutego.date4u.core.profile.Profile;
@@ -12,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +25,7 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -62,11 +65,12 @@ public class Date4uWebController {
     public String searchPage(Model model,
                              @ModelAttribute("filter") FilterFormData filter,
                              @RequestParam(defaultValue="1") int page,
-                             @RequestParam(defaultValue = "5") int size){
+                             @RequestParam(defaultValue = "5") int size,
+                             Authentication auth){
 
         log.info("number of page: " + page + " , Input parameters: " + filter.getGender() + " and: " + filter.getMaxAge() +  " and: " + filter.getMinAge() );
         log.info("filter id: " + filter);
-        Page<Profile> pagesOfProfiles =  profileService.findPaginated(PageRequest.of(page-1, 5),  filter);
+        Page<Profile> pagesOfProfiles =  profileService.findPaginated(PageRequest.of(page-1, 5),  filter, getOwnId(auth));
         int totalPages = pagesOfProfiles.getTotalPages();
         if(pagesOfProfiles.getTotalPages()==0){
             //  attributes.addAttribute("msg", "No unicorn profiles were found based on the specified parameters.");
@@ -78,10 +82,6 @@ public class Date4uWebController {
             model.addAttribute("message","Invalid page number." );
             return "search";
         }
-        
-
-//        log.info("gender: " + gender + " minAge: " + "maxAge: ");
-       
        
         List<ProfileFormData> listOfProfileDTO = profileService.convertProfileToProfileDTO(pagesOfProfiles.getContent());
         model.addAttribute("pageOfProfiles", listOfProfileDTO);
@@ -100,71 +100,19 @@ public class Date4uWebController {
         
         return "search";
     }
-//    @GetMapping({"/search/result", "/search"})
-//    public String getSearchResult(Model model,
-//                                  @RequestParam(defaultValue="1") int page,
-//                                  @RequestParam(defaultValue = "5") int size,
-//                                  @RequestParam(defaultValue = "1", required = false) int gender,
-//                                  @RequestParam(defaultValue = "18", required = false) int minAge,
-//                                  @RequestParam(defaultValue = "99", required = false) int maxAge,
-//                                  @RequestParam(defaultValue = "0", required = false) int minLength,
-//                                  @RequestParam(defaultValue = "100", required = false) int maxLength,
-//                                  RedirectAttributes attributes){
-//
-//
-//        Page<Profile> pagesOfProfiles =  profileService.findPaginated(PageRequest.of(page-1, 5), gender, minAge, maxAge);
-////        log.info("gender: " + gender + " minAge: " + "maxAge: ");
-//        int totalPages = pagesOfProfiles.getTotalPages();
-//        if(page > totalPages){
-//            attributes.addFlashAttribute("message", "This page number is not valid.");
-//            return "redirect:/search";
-//        }
-//        List<ProfileFormData> listOfProfileDTO = profileService.convertProfileToProfileDTO(pagesOfProfiles.getContent());
-//        model.addAttribute("pageOfProfiles", listOfProfileDTO);
-//        model.addAttribute("currentPage", page);
-//        model.addAttribute("totalPages", totalPages);
-//        model.addAttribute("totalProfilesNumber", pagesOfProfiles.getTotalElements());
-//
-//        model.addAttribute("filter", new FilterFormData());
-//
-//        if (totalPages > 0) {
-//            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
-//                    .boxed()
-//                    .collect(Collectors.toList());
-//            model.addAttribute("pageNumbers", pageNumbers);
-//        }
-//
-//        return "search";
-//
-//    }
     
-//    @GetMapping( "/search" )
-//    public String searchPage(Model model,
-//                             @RequestParam(defaultValue="1") int page,
-//                             @RequestParam(defaultValue = "5") int size, RedirectAttributes attributes){
-//
-//        Page<Profile> pagesOfProfiles =  profileService.findPaginated(PageRequest.of(page-1, 5));
-//
-//        int totalPages = pagesOfProfiles.getTotalPages();
-//        if(page > totalPages){
-//            attributes.addFlashAttribute("message", "This page number is not valid.");
-//            return "redirect:/search";
-//        }
-//        List<ProfileFormData> listOfProfileDTO = profileService.convertProfileToProfileDTO(pagesOfProfiles.getContent());
-//        model.addAttribute("pageOfProfiles", listOfProfileDTO);
-//        model.addAttribute("currentPage", page);
-//        model.addAttribute("totalPages", totalPages);
-//        model.addAttribute("totalProfilesNumber", pagesOfProfiles.getTotalElements());
-//
-//        if (totalPages > 0) {
-//            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
-//                    .boxed()
-//                    .collect(Collectors.toList());
-//            model.addAttribute("pageNumbers", pageNumbers);
-//        }
-//
-//        return "search";
-//    }
+    
+    private Optional<Long> getOwnId(Authentication auth){
+        CurrentUser unicorn = null;
+        
+        if(auth != null) {
+            unicorn = (CurrentUser) auth.getPrincipal();
+            return Optional.of(unicorn.getProfile().getId());
+        }
+        
+        return Optional.empty();
+    }
+
     
    
     
